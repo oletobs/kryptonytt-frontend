e4606b9295d715509d78ef5e3351adf07ebafacd
Ole Tobias Strøm <oletobs@stud.ntnu.no>
2017-11-06 20:31:09 +0100

    change public path

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: ['./src/assets/scss/app.scss','./src/main.js'],
     output: {
         path: path.resolve(__dirname, './dist'),
-        publicPath: '/dist/',
+        publicPath: '/',
         filename: 'js/app.bundle.js'
     },
     module: {
